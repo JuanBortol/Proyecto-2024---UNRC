8d6d65864072fc739855c99b054d40a934b48f6c
Macr0os <matiasnfsw114@gmail.com>
2024-09-20 12:50:23 -0300

    agregados archivos y cambios menores

--- Proyecto2024/modelo/gradio.py
+++ Proyecto2024/modelo/gradio.py
@@ -2,24 +2,22 @@
 from io import TextIOWrapper
 from typing import TextIO
 
-from gradio_client import Client, file
+from gradio_client import Client, handle_file
 import glob
 
 client = Client("https://5b8add2a5d98b1a652ea7fd72d942dac.app-space.dplink.cc/")
 
-receptor_files = glob.glob('*.ent')
-
+receptor_files = ['hy.pdb']
 resultPocket = client.predict(
-    ligand_file=file('1G9V_ligand.sdf'),
+    ligand_file=handle_file('Structure2D_COMPOUND_CID_4696.sdf'),
     expand_size=10,
     api_name="/get_pocket_by_ligand"
 )
-
 for receptor_file in receptor_files:
     print(receptor_file)
     resultDocking = client.predict(
-        receptor_pdb=file(receptor_file),
-        ligand_sdf=file('AFB1_ligand.sdf'),
+        receptor_pdb=handle_file(receptor_file),
+        ligand_sdf=handle_file('Structure2D_COMPOUND_CID_4696.sdf'),
         center_x=resultPocket[0],
         center_y=resultPocket[1],
         center_z=resultPocket[2],
@@ -34,14 +32,18 @@ for receptor_file in receptor_files:
 
     a, b, c, d = resultDocking
 
+    print(c['value']) #codigo html
+
     try:
         file_path = b['value']
     except (TypeError, KeyError):
         print('la proteina ' + receptor_file + ' no hace docking')
+        print('------------------------------------------------------------')
         continue
 
     with open(file_path, 'r') as file:
         for line in file:
             if line.startswith(">  <docking_score>"):
                 docking_score = file.readline().strip()
-                print('la proteina ' + receptor_file + docking_score)
+                print('la proteina ' + receptor_file + ' hace docking con: ' +docking_score)
+                print('------------------------------------------------------------')
