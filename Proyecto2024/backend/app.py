8168e51950673c85e5ac71523537e4f84906f703
Ricardo Trimboli <riki.trimboli@gmail.com>
2024-11-10 18:33:07 -0300

    modificacion de app para el uso del controlador usuario

--- Proyecto2024/backend/app.py
+++ Proyecto2024/backend/app.py
@@ -15,6 +15,7 @@ import secrets
 import os
 import numpy as np
 import tensorflow as tf
+from controllers.user_controller import register, login, logout
 
 
 app = Flask(__name__)
@@ -231,56 +232,16 @@ def submit_model():
 
 
 @app.route("/register", methods=['POST'])
-def register():
-    if request.method == 'POST':
-        data = request.get_json()
-        username = data.get('username')
-        password = data.get('password')
-        confirm_password = data.get('confirm_password')
-
-        if not username or not password:
-            return jsonify({"message": "Todos los campos son obligatorios"}), 400
-        elif password != confirm_password:
-            return jsonify({"message": "Las contraseñas no coinciden"}), 400
-        else:
-            # User already exists?
-            existing_user = db_session.query(User).filter_by(username=username).first()
-            if existing_user:
-                return jsonify({"message": "El usuario ya existe"}), 400
-            else:
-                nuevo_usuario = User(username=username, password=password)
-                db_session.add(nuevo_usuario)
-                db_session.commit()
-                return jsonify({"message": "Usuario registrado con éxito"}), 200
-
-    return jsonify({"message": "Método no permitido"}), 405
+def register_route():
+    return register()
 
 @app.route("/login", methods=['POST'])
-def login():
-    if 'user_id' in session:
-        return jsonify({'message': 'User already logged in'}), 200
-    
-    data = request.get_json()
-    username = data['username']
-    password = data['password']
-
-    user = db_session.query(User).filter_by(username=username, password=password).first()
-
-    if not user:
-        return jsonify({'message': 'Usuario o contraseña incorrectos'}), 400
-
-    session['user_id'] = user.id
-    session['username'] = user.username
-
-    return jsonify({
-        "user_id": user.id,
-        "username": user.username
-        }), 200
+def login_route():
+    return login()
 
 @app.route('/logout', methods=['POST'])
-def logout():
-    session.clear()
-    return jsonify({'message': 'Logged out successfully'}), 200
+def logout_route():
+    return logout()
 
 @app.route("/submit_report", methods=['POST'])
 def submit_report():
