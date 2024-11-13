3e4d051608c78bcc7326033d2ca268238f4203a0
Nicolás Centeno <72638766+nikocent@users.noreply.github.com>
2024-11-13 01:45:54 -0300

    Mini fix en httpClient

--- Proyecto2024/frontend/src/utils/httpClient.js
+++ Proyecto2024/frontend/src/utils/httpClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 const httpClient = axios.create({
   baseURL: apiUrl,
